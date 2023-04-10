40c42e6bb382e8e2219f531993b37a310e3fb321
nicholasoharvey <nicholas.o.harvey@gmail.com>
2023-04-10 14:01:26 -0700

    Assigment 3 part 2wq

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,6 +12,7 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+PATH=$PATH:$FINDER_APP_DIR/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin
 
 if [ $# -lt 1 ]
 then
@@ -22,22 +23,37 @@ else
 fi
 
 mkdir -p ${OUTDIR}
-
+NUMPROC=`nproc`
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+	git clone ${KERNEL_REPO} --single-branch --branch ${KERNEL_VERSION}
 fi
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
+    #the files to patch are only generated during build 
+    make -i -j${NUMPROC} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -i -j${NUMPROC} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    cp "${FINDER_APP_DIR}"/yylloc.patch . 
+    git apply --check yylloc.patch
+    result=$?
+    if [ "$result" -eq 0 ]; then
+        patch -p1 < yylloc.patch
+    else
+        echo "Issue applying patch"
+    fi 
+    make -j${NUMPROC} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j${NUMPROC} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make -j${NUMPROC} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make -j${NUMPROC} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 
-    # TODO: Add your kernel build steps here
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,6 +64,10 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +76,47 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make -j${NUMPROC} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make -j${NUMPROC} CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
-
+export SYSROOT=`${CROSS_COMPILE}gcc --print-sysroot`
+cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+cp ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
 # TODO: Make device nodes
-
-# TODO: Clean and build the writer utility
-
-# TODO: Copy the finder related scripts and executables to the /home directory
+cd "${OUTDIR}/rootfs"
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
+#  Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+make clean
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+cp ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home
+# Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-
-# TODO: Chown the root directory
-
-# TODO: Create initramfs.cpio.gz
+cp ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home
+cp -r ${FINDER_APP_DIR}/conf/ ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home
+# Chown the root directory
+cd "${OUTDIR}/rootfs"
+sudo chown -R root:root * 
+#  Create initramfs.cpio.gz
+cd "${OUTDIR}/rootfs"
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd "${OUTDIR}"
+gzip -f initramfs.cpio
